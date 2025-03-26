7f088ed48ac97ab0325073dcd38e7dfcdebc3c59
yanis <yanis.temmim@reseau.eseo.fr>
2025-03-26 16:55:18 +0100

    ajout player.c/h et modif main.c

---
+++ base/player.c
@@ -0,0 +1,80 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include "getch.h"
+#include "map.h"
+#include "player.h"
+
+struct player_s {
+    int posx;
+    int posy;   
+};
+
+extern Player* player_new(){
+    this = (Player*)calloc(1,sizeof(Player));
+    // il faut tester s'il y a une erreur ici même si c'est pas censé
+    return this;
+}
+    
+extern void player_init(Player* player, char **map){
+    player->posx= 0;
+    player->posy = 0;
+    set_case(map,this->posx,this->posy)= 'j';
+}  
+
+extern bool movement(Player* this, char **map, char direction){
+    //tester si le mouvement est valide
+    
+    switch(direction)
+    set_case(map,this->posx,this->posy)= ' ';
+        {
+            case 'q':
+                fin = 1;
+                break;
+            case 'i':
+                //verif dépassement
+                if (this->posx-1<0){
+                    set_case(map,this->posx,this->posy)= 'j';
+                    return 0
+                }
+                this->posx--; 
+                break;
+            case 'k':
+                //verif dépassement
+                if (this->posx+1>get_colonne(map)){
+                    set_case(map,this->posx,this->posy)= 'j';
+                    return 0
+                }
+                this->posx++; 
+                break;
+            case 'j':
+                //verif dépassement
+                if (this->posy-1<0){
+                    set_case(map,this->posx,this->posy)= 'j';
+                    return 0
+                }
+                this->posy--; 
+                break;
+            case 'l':
+                //verif dépassement
+                if (this->posx+1>get_ligne(map)){    
+                    set_case(map,this->posx,this->posy)= 'j';
+                    return 0
+                }
+                this->posy++; 
+                break;
+        }
+    
+    set_case(map,this->posx,this->posy)= 'j';
+    return 1
+}
+
+extern char* get_pos(Player* this){
+    static char pos[2];  // Utilisation d'un tableau statique
+    pos[0] = this->posx;
+    pos[1] = this->posy
+    return pos;
+}
+
+
+
---
+++ base/player.h
@@ -0,0 +1,13 @@
+#ifndef PLAYR_H
+#define PLAYER_H
+
+#include <stdint.h> // uint8_t
+#include "getch.h"
+#include "map.h"
+
+
+typedef struct player_s Player;
+extern bool movement(Player* this, char direction);  
+extern void player_s* init_p(Map* map);
+extern char* get_pos(Player* this);
+#endif
